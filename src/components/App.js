47a8737287313c0460cf2de143005956a100ef24
Roman Stix <roman.stix@digitalsunray.com>
2019-05-06 16:10:47 +0200

    fetching data

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit = term => {
-    console.log(term);
+  state = { images: [] };
+
+  onSearchSubmit = async term => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: {
+        query: term
+      },
+      headers: {
+        Authorization:
+          'Client-ID 082444e25704f181e76695785d2cfcadf6a000293d78bf1aa6b1018672565cb3'
+      }
+    });
+    console.log(this);
+    this.setState({ images: response.data.results });
   };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length}
       </div>
     );
   }
